e44f4378be50e004439f72612d869c5aec9bd66d
Asadbek <asadbekabdumajidov7@gmail.com>
2022-03-16 19:29:35 +0500

    remote

--- lib/services/service_share.dart
+++ lib/services/service_share.dart
@@ -4,7 +4,7 @@ class Servicesharre {
   static Future<void> share() async {
     await FlutterShare.share(
         title: "Ispnacha so'zlashgich",
-        linkUrl: 'https://t.me/asadbek_blog1',
+        linkUrl: 'https://t.me/asadbek_blog1/143',
         chooserTitle: "Ispnacha so'zlashgich");
   }
 }
