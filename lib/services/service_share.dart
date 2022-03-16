1227036efdb13702c86217c7e3b8917aa158c229
Asadbek <asadbekabdumajidov7@gmail.com>
2022-03-16 23:55:34 +0500

    remote

--- lib/services/service_share.dart
+++ lib/services/service_share.dart
@@ -3,8 +3,8 @@ import 'package:flutter_share/flutter_share.dart';
 class Servicesharre {
   static Future<void> share() async {
     await FlutterShare.share(
-        title: "Ispnacha so'zlashgich",
+        title: "Namoz najotdir",
         linkUrl: 'https://t.me/asadbek_blog1/143',
-        chooserTitle: "Ispnacha so'zlashgich");
+        chooserTitle: "Namoz najotdir");
   }
 }
